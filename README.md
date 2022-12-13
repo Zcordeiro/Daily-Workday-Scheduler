c3e7b70d0a87745d688763d302c4c0702bfaecea
zcord <zeus.cordeiro@gmail.com>
2022-12-12 21:44:03 -0500

    Finished JS file and added README info

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # daily-tasks-list
+This was a daily task list we were asked to create.
+
+## Information
+On this webpage you'll be able to fill in tasks and save them to the differnt time frames. It will also highlight the current time and show you the times that have passed in gray and the future hours in red
+
+---
+
+## Screenshot of finished page 
+![screenshot of portfolio page](./Screen-shot.png)
+
+---
+
+## Link to github pages and repo
+REPO - https://github.com/Zcordeiro/daily-tasks-list
+
+Published Page - 
--- script.js
+++ script.js
@@ -1,34 +1,72 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).function() {
+$(document).ready(function () {
+    var currentDate = $('#currentDay');
+  
+    function displayDate() {
+      var date = dayjs().format('dddd MMM, DD YYYY');
+      currentDate.text(date);
+      var time = dayjs().format('hh:mm a');
+      var timeDisplay = $('<p>');
+      timeDisplay.text(time);
+      currentDate.append(timeDisplay);
+    }
     
+    $('.saveBtn').click(function () {
+      var taskText = $(this).siblings('.taskEntry').val().trim();
+      var taskTime = $(this).parent().attr('id');
+      localStorage.setItem(taskTime, taskText);
+  
+  
+      console.log('save btn clicked');
+      console.log(taskText, taskTime);
+    });
+  
+  
+    
+  
+    function timeColorChange() {
+      var currentHour = dayjs().hour();
+  
+      $('.time-block').each(function () { 
+        var taskTime = $(this).attr('id');
+  
+        if(taskTime == currentHour) {
+          $(this).addClass('present');
+        } else if (taskTime < currentHour) {
+          $(this).addClass('past');
+        } else {
+          $(this).addClass('future');
+        }
+  
+      console.log('current task hour for Color change: ', taskTime);
+      }
+      )
+  
+      console.log('currentHour for Color change: ', currentHour);
+    }
+  
+    timeColorChange()
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-  
-  
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-  
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  //Displays the locally stored tasks in the text boxes.   
+    $('#h7').text(localStorage.getItem('7'));
+    $('#h8').text(localStorage.getItem('8'));
+    $('#h9').text(localStorage.getItem('9'));
+    $('#h10').text(localStorage.getItem('10'));
+    $('#h11').text(localStorage.getItem('11'));
+    $('#h12').text(localStorage.getItem('12'));
+    $('#h13').text(localStorage.getItem('13'));
+    $('#h14').text(localStorage.getItem('14'));
+    $('#h15').text(localStorage.getItem('15'));
+    $('#h16').text(localStorage.getItem('16'));
+    $('#h17').text(localStorage.getItem('17'));
+    $('#h18').text(localStorage.getItem('18'));
   
   
   
   
-  };
+  
+    displayDate();
+    setInterval(displayDate, 1000);
+  });
   
   
   
